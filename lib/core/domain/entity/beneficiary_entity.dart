9110475f97dc908c6df2453f9313404a50fbb052
beso-pro <basel.hjz1999@gmail.com>
2024-08-12 17:52:00 +0400

    Riverpod implementation.

--- lib/core/domain/entity/beneficiary_entity.dart
+++ lib/core/domain/entity/beneficiary_entity.dart
@@ -14,4 +14,12 @@ class BeneficiaryEntity {
         nickName: json['nick_name'],
         phoneNumber: json['phone_number']);
   }
+
+  Map<String, dynamic> toJson(BeneficiaryEntity beneficiaryEntity) {
+    return {
+      'id': beneficiaryEntity.id,
+      'nick_name': beneficiaryEntity.nickName,
+      'phone_number': beneficiaryEntity.phoneNumber
+    };
+  }
 }
--- lib/features/add_new_beneficiary/domain/repository/add_new_beneficiary_repository.dart
+++ lib/features/add_new_beneficiary/domain/repository/add_new_beneficiary_repository.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import '../../../../core/data/repository/base_repository.dart';
 import '../../../../core/domain/entity/beneficiary_entity.dart';
 import '../../../../core/error/failures.dart';
